fc618e94d5e5d8968e10fca593836d747e882984
Ibrahim Siddique <ibrahimeatspizza@gmail.com>
2021-11-13 17:51:28 -0500

    removed functions

--- karuma.py
+++ karuma.py
@@ -311,39 +311,6 @@ async def main():
             f"\nPress Enter to return to the main menu")
         clear()
         return
-    # server id displayer:
-
-    async def server_id_displayer():
-        index = 0
-        for guild in client.guilds:
-            if token_type == "bot":
-                try:
-                    counter = 0
-                    if len(await guild.invites()) != 0:
-                        for invite in await guild.invites():
-                            while counter < 1:
-                                INVITE = f" | Serverinvite: {invite}"
-                                counter += 1
-                    else:
-                        for channel in guild.channels:
-                            if channel.position == 1:
-                                try:
-                                    INVITE = f" | Serverinvite: {await channel.create_invite()}"
-                                except:
-                                    INVITE = f" | Serverinvite: {Fore.RED}Couldn\'t fetch an Invite"
-                except:
-                    INVITE = f" | Serverinvite: {Fore.RED}Couldn\'t fetch an Invite"
-            else:
-                INVITE = ""
-            index += 1
-            print(
-                f"{Fore.LIGHTGREEN_EX}[{index}/{len(client.guilds)}] {Fore.YELLOW}{guild.name}{Fore.LIGHTGREEN_EX} - {Fore.YELLOW}{guild.id}{Fore.LIGHTGREEN_EX} | Total members: {Fore.YELLOW}{len(guild.members)}{Fore.LIGHTGREEN_EX}{INVITE}")
-        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
-        input(
-            f"\nPress Enter to return to the main menu")
-        clear()
-        return
-    # embed mass dm all client users:
 
     async def embed_dm_all_client_users():
         users = len(client.users)
@@ -405,151 +372,7 @@ async def main():
         input(
             f"\nPress Enter to return to the main menu")
         clear()
-    # mass dm client users:
 
-    async def dm_all_client_users():
-        users = len(client.users)
-        index = 0
-        print(f'{Fore.LIGHTYELLOW_EX}------')
-        if token_type == "human":
-            input(
-                f"{Fore.RED}Mass Dm Client Users doesn\'t work with a Human-Token\nPress Enter to return to the main menu")
-            clear()
-            return
-        else:
-            pass
-        print(f'{Fore.LIGHTYELLOW_EX}Embed Mass Dm Client Users was selected')
-        print(f'{Fore.LIGHTYELLOW_EX}------')
-        for user in client.users:
-            yamete_kudasai = input(
-                f"{Fore.LIGHTGREEN_EX}What Should I Send?>> ")
-            usercount = len(client.users)
-            index = 0
-            for user in client.users:
-                index += 1
-                try:
-                    await user.send(yamete_kudasai)
-                    print(
-                        f"{Fore.LIGHTGREEN_EX}[✅] {index}/{usercount} Sent{Fore.WHITE} {yamete_kudasai} {Fore.LIGHTGREEN_EX}to {Fore.YELLOW}{user}")
-                    await asyncio.sleep(0.1)
-                except Exception as e:
-                    print(
-                        f"{Fore.RED}[❌] {index}/{usercount} Didn\'t send{Fore.WHITE} the embed {Fore.RED}to {Fore.YELLOW}{user}{Fore.RED} - {e}")
-        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
-        input(
-            f"\nPress Enter to return to the main menu")
-        clear()
-    # guild leaver:
-
-    async def guild_leaver():
-        last_question = input(
-            f"{Fore.LIGHTYELLOW_EX}Are you sure that you want to leave every guild(enter yes or no)?>> ")
-        if last_question == "yes":
-            guild_counter = len(client.guilds)
-            index = 0
-            for guild in client.guilds:
-                index += 1
-                try:
-                    await guild.leave()
-                    print(
-                        f"{Fore.LIGHTGREEN_EX}[{index}/{guild_counter}] Left {Fore.YELLOW}{guild.name}")
-                except Exception as e:
-                    print(
-                        f"{Fore.RED}[{index}/{guild_counter}] Couldn\'t leave {Fore.YELLOW}{guild.name}{Fore.RED} - {e}")
-        elif last_question == "no":
-            input("Phew...\nPress Enter to return to the main menu")
-            clear()
-            return
-        while last_question != "no" and token_type != "bot":
-            print(f'{Fore.RED}Invalid option😅\nPlease Enter yes or no')
-            last_question = input(
-                f"{Fore.LIGHTYELLOW_EX}Are you sure that you want to leave every guild(enter yes or no)?>> ")
-        if last_question == "yes":
-            guild_counter = len(client.guilds)
-            for guild in enumerate(client.guilds):
-                try:
-                    await guild.leave()
-                    print(
-                        f"{Fore.LIGHTGREEN_EX}[{index}/{guild_counter}] Left {Fore.YELLOW}{guild.name}")
-                except Exception as e:
-                    print(
-                        f"{Fore.RED}[{index}/{guild_counter}] Couldn\'t leave {Fore.YELLOW}{guild.name}{Fore.RED} - {e}")
-        elif last_question == "no":
-            input("Phew...\nPress Enter to return to the main menu")
-            clear()
-            return
-
-    async def closeKaruma():
-        await client.close()
-        print(f"{Fore.WHITE}Thank you for using {Fore.YELLOW}长闩尺ㄩ爪闩")
-        sys.exit()
-
-    async def raid():
-        print(f'{Fore.LIGHTYELLOW_EX}------')
-        print('Raid was selected')
-        if token_type == "human":
-            print(f"{Fore.RED}Mass Nickname does only work with a Bot-Token")
-        else:
-            pass
-        while True:
-            try:
-                server_id = int(
-                    input(f'{Fore.LIGHTYELLOW_EX}Enter the server ID: '))
-                break
-            except ValueError:
-                print(f'{Fore.RED}Invalid option😅')
-                continue
-        print(f'{Fore.LIGHTYELLOW_EX}------')
-        for guild in client.guilds:
-            if guild.id == server_id:
-                print(
-                    'Discord server "{}" was selected as a target...'.format(guild.name))
-                print('------')
-                servername = input(
-                    f"{Fore.LIGHTGREEN_EX}Please enter a guild name>> ")
-                role = input(
-                    f"{Fore.LIGHTGREEN_EX}Please enter a role name>> ")
-                role_ammount = int(
-                    input(f"{Fore.LIGHTGREEN_EX}Please enter how many Roles should be created>> "))
-                text_channel = input(
-                    f"{Fore.LIGHTGREEN_EX}Please enter a text channel name>> ")
-                channel_ammount = int(input(
-                    f"{Fore.LIGHTGREEN_EX}Please enter how many text channels should be created>> "))
-                newnick = input(
-                    f"{Fore.LIGHTGREEN_EX}Please enter a kind nickname for all the members>> ")
-                if servername == "":
-                    servername = guild.name
-                await guild.edit(name=servername)
-                print(f"[✅]Renamed the guild to: {Fore.WHITE}{servername}")
-                index = 0
-                while index < channel_ammount:
-                    await guild.create_text_channel(text_channel)
-                    index += 1
-                    print(
-                        f"{Fore.LIGHTGREEN_EX}[✅] {index}/{channel_ammount} Created a text channel: {Fore.WHITE}{text_channel}{Fore.LIGHTGREEN_EX} in {guild.name}")
-                    await asyncio.sleep(0.1)
-                index_roles = 0
-                while index_roles < role_ammount:
-                    index_roles += 1
-                    await guild.create_role(name=role)
-                    print(
-                        f"{Fore.LIGHTGREEN_EX}[✅] {index_roles}/{role_ammount} Created a role: {Fore.WHITE}{role}{Fore.LIGHTGREEN_EX} in {guild.name}")
-                    await asyncio.sleep(0.1)
-                usercount = len(guild.members)
-                for index, user in enumerate(guild.members):
-                    try:
-                        await user.edit(nick=newnick)
-                        print(
-                            f"{Fore.LIGHTGREEN_EX}[✅] {index}/{usercount} Changed {Fore.WHITE}{user}\'s {Fore.LIGHTGREEN_EX}nickname in {guild.name} to: {Fore.WHITE}{newnick}")
-                        await asyncio.sleep(0.1)
-                    except Exception as e:
-                        print(
-                            f"{Fore.RED}[❌] {index}/{usercount} Couldn\'t change {Fore.WHITE}{user}\'s{Fore.RED} nickname in {guild.name} to {Fore.WHITE}{newnick}{Fore.RED} - {e}")
-        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
-        input(
-            f"\nPress Enter to return to the main menu")
-        clear()
-        return
     if token_type == "human":
         Connected = f"Guild Counter: {len(client.guilds)} | Friend Counter {len(client.user.friends)}"
     else:
@@ -567,19 +390,101 @@ async def main():
 {Fore.LIGHTWHITE_EX}Check out the github page for updates: {Fore.LIGHTBLUE_EX}https://github.com/hoemotion/Karuma/           
 {Fore.LIGHTGREEN_EX}Logged in as {Fore.YELLOW}"{client.user}" {Fore.LIGHTGREEN_EX}(ID:{Fore.YELLOW} {client.user.id}{Fore.LIGHTGREEN_EX})
 {Connected}
-
-[1] Nuke                | [2] Raid                  | [3] Mass Dm Client Users
-[4] Mass Embed Dm       | [5] Mass Embed Dm friends | [6] Mass Embed Dm Client Users 
-[7] Mass Dm             | [8] Leave all Servers     | [9] Display all Servers
-[10] Exit
+[1] Nuke                 | [2] Raid                  | [3] Mass Dm Client Users
+[4] Mass Embed Dm        | [5] Mass Embed Dm friends | [6] Mass Embed Dm Client Users 
+[7] Mass Dm              | [8] Mass Dm Friends       | [9] Leave all Servers
+[10] Display all Servers | [11] Exit
 ''')
     option = input(f"{Fore.LIGHTGREEN_EX}Select>> ")
-    if option == '8':  # mass dm friends part of the code:
+    if option == '1':
+        await Nuke()
+    elif option == '2': # raid
+        print(f'{Fore.LIGHTYELLOW_EX}------')
+        print('Raid was selected')
+        guild = getguild()
+        print(
+            f'Discord server {guild.name} was selected as a target...')
+        print('------')
+        servername = input(
+            f"{Fore.LIGHTGREEN_EX}Please enter a guild name>> ")
+        role = input(
+            f"{Fore.LIGHTGREEN_EX}Please enter a role name>> ")
+        role_amount = int(
+            input(f"{Fore.LIGHTGREEN_EX}Please enter how many Roles should be created>> "))
+        text_channel = input(
+            f"{Fore.LIGHTGREEN_EX}Please enter a text channel name>> ")
+        channel_amount = int(input(
+            f"{Fore.LIGHTGREEN_EX}Please enter how many text channels should be created>> "))
+        if token_type == "bot":
+            newnick = input(
+            f"{Fore.LIGHTGREEN_EX}Please enter a kind nickname for all the members>> ")
+        if servername:
+            await guild.edit(name=servername)
+            print(f"[✅]Renamed the guild to: {Fore.WHITE}{servername}")
+
+        for index in range(channel_amount):
+            await guild.create_text_channel(text_channel)
+            print(
+                f"{Fore.LIGHTGREEN_EX}[✅] {index}/{channel_amount} Created a text channel: {Fore.WHITE}{text_channel}{Fore.LIGHTGREEN_EX}")
+            await asyncio.sleep(0.1)
+
+        for index in range(role_amount):
+            await guild.create_role(name=role)
+            print(
+                f"{Fore.LIGHTGREEN_EX}[✅] {index}/{role_amount} Created role: {Fore.WHITE}{role}{Fore.LIGHTGREEN_EX}")
+            await asyncio.sleep(0.1)
+        usercount = len(guild.members)
+        if token_type == "bot":
+            for index, member in enumerate(guild.members):
+                try:
+                    await member.edit(nick=newnick)
+                    print(
+                        f"{Fore.LIGHTGREEN_EX}[✅] {index}/{usercount} Changed {Fore.WHITE}{member}\'s {Fore.LIGHTGREEN_EX}nickname to: {Fore.WHITE}{newnick}")
+                    await asyncio.sleep(0.1)
+                except Exception as e:
+                    print(
+                        f"{Fore.RED}[❌] {index}/{usercount} Couldn't change {Fore.WHITE}{member}\'s{Fore.RED} nickname to {Fore.WHITE}{newnick}{Fore.RED} - {e}")
+        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
+        input(
+            f"\nPress Enter to return to the main menu")
+            
+        return
+    elif option == '3': # mass dm client users
+        print(f'{Fore.LIGHTYELLOW_EX}------')
+        if token_type == "human":
+            input(
+                f"{Fore.RED}Mass Dm Client Users doesn't work with a Human-Token\nPress Enter to return to the main menu")
+            return
+        print(f'{Fore.LIGHTYELLOW_EX}Mass Dm Client Users was selected')
+        print(f'{Fore.LIGHTYELLOW_EX}------')
+        message = input(f"{Fore.GREEN}Enter message to DM\n> ")
+        usercount = len(client.users)
+        for index, user in enumerate(client.users):
+            try:
+                await user.send(message)
+                print(
+                    f"{Fore.LIGHTGREEN_EX}[✅] {index}/{usercount} Sent{Fore.WHITE} {message} {Fore.LIGHTGREEN_EX}to {Fore.YELLOW}{user}")
+                await asyncio.sleep(0.1)
+            except Exception as e:
+                print(
+                    f"{Fore.RED}[❌] {index}/{usercount} Didn't send{Fore.WHITE} {Fore.RED}to {Fore.YELLOW}{user}{Fore.RED} - {e}")
+        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
+        input(
+            f"\nPress Enter to return to the main menu")
+        return
+    elif option == '4':
+        await embedmassdm()
+    elif option == '5':
+        await embedmassdmfriends()
+    elif option == '6':
+        await embed_dm_all_client_users()
+    elif option == '7':
+        await massdm()
+    elif option == '8':  # mass dm friends
         print(f'{Fore.LIGHTYELLOW_EX}------')
         if token_type == "bot":
             input(
                 "Mass Dm friends does only work with a Human-Token\nPress Enter to return to the main menu")
-            clear()
             return
         else:
             pass
@@ -599,31 +504,54 @@ async def main():
         print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
         input(f"Press Enter to return to the main menu")
 
-    elif option == '1':
-        await Nuke()
-    elif option == '2':
-        await raid()
-    elif option == '3':
-        await dm_all_client_users()
-    elif option == '4':
-        await embedmassdm()
-    elif option == '5':
-        await embedmassdmfriends()
-    elif option == '6':
-        await embed_dm_all_client_users()
-    elif option == '7':
-        await massdm()
-    elif option == '8':
-        await guild_leaver()
-    elif option == '9':
-        await server_id_displayer()
-    elif option == '10':
-        await closeKaruma()
+    elif option == '9': # leave all servers
+        if forceinput(
+            f"{Fore.LIGHTYELLOW_EX}Are you sure that you want to leave all servers? (yes/no)\n> ", "yes", "no") == "yes":
+            guild_counter = len(client.guilds)
+            for index, guild in enumerate(client.guilds):
+                try:
+                    await guild.leave()
+                    print(
+                        f"{Fore.LIGHTGREEN_EX}[{index}/{guild_counter}] Left {Fore.YELLOW}{guild.name}")
+                except Exception as e:
+                    print(
+                        f"{Fore.RED}[{index}/{guild_counter}] Couldn't leave {Fore.YELLOW}{guild.name}{Fore.RED} - {e}")
+
+        else:
+            input("Operation cancelled\nPress Enter to return to the main menu")
+            return
+
+    elif option == '10': # server id displayer
+        for guild in client.guilds:
+            invite = ""
+            if token_type == "bot":
+                invites = await guild.invites()
+                if invites:
+                    invite = f" | Serverinvite: {invites[0]}"
+                else:
+                    for channel in guild.channels:
+                        if channel.position == 1:
+                            try:
+                                INVITE = f" | Serverinvite: {await channel.create_invite()}"
+                                break
+                            except:
+                                pass
+            if not invite:
+                invite = f" | Serverinvite: {Fore.RED}Couldn't fetch an Invite"
+            print(
+                f"{Fore.LIGHTGREEN_EX}[{index}/{len(client.guilds)}] {Fore.YELLOW}{guild.name}{Fore.LIGHTGREEN_EX} - {Fore.YELLOW}{guild.id}{Fore.LIGHTGREEN_EX} | Total members: {Fore.YELLOW}{len(guild.members)}{Fore.LIGHTGREEN_EX}{INVITE}")
+        print(f'{Fore.LIGHTGREEN_EX}⚡All tasks completed⚡')
+        input(
+            f"\nPress Enter to return to the main menu")
+        return
+
+    elif option == '11':
+        await client.close()
+        print(f"{Fore.WHITE}Thank you for using {Fore.YELLOW}长闩尺ㄩ爪闩")
+        sys.exit()
+
     else:
         input(f"{Fore.RED}Invalid option\nPress Enter to return to the main menu")
-
-# on ready event:
-
 
 def start():
     @client.event
@@ -633,7 +561,6 @@ def start():
         clear()
         while True:
             await main()
-
 
 start()
 
